cba330eef080de8647f4fd90ee010f2321ae6c5e
Daniil Yavorskiy <79133931515@yandex.ru>
2022-03-21 13:05:15 +0700

    Fix

---
+++ src/geometry/geometry.c
@@ -0,0 +1,26 @@
+#include "libgeo/circle.h"
+#include "libgeo/triangle.h"
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    while (1) {
+        printf("1. Cirle\n2. Triangle\n3. Poligon\nChoose your figure: ");
+        scanf(" %d", &a);
+        if (a == 1) {
+            circle();
+            break;
+        }
+        if (a == 2) {
+            triangle();
+            break;
+        }
+        else {
+            printf("Error\n");
+            return 0;
+        }
+    }
+    return 0;
+}
